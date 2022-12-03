4214834257ae4b49961cc17269313b714ffead2d
Aamir Farooq <aamir@farooq.xyz>
2022-12-03 22:55:36 +0000

    Added fix instagram

---
+++ cogs/monitors/misc/fixsocials.py
@@ -0,0 +1,63 @@
+import asyncio
+import re
+
+import discord
+from data.services import guild_service
+from discord.ext import commands
+from utils import cfg
+
+
+class FixSocials(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        # regex for tiktok urls
+        self.tiktok_pattern = re.compile(r"https:\/\/(www.)?((vm|vt).tiktok.com\/[A-Za-z0-9]+|tiktok.com\/@[\w.]+\/video\/[\d]+\/?|tiktok.com\/t\/[a-zA-Z0-9]+\/)")
+
+        # regex for instagram urls
+        self.instagram_pattern = re.compile(r"(https:\/\/(www.)?instagram\.com\/(?:p|reel)\/([^/?#&]+))\/")
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if cfg.aaron_id is None or cfg.aaron_role is None:
+            return
+        if not message.guild:
+            return
+        if message.guild.id != cfg.guild_id:
+            return
+        if message.author.bot:
+            return
+        if message.channel.id != guild_service.get_guild().channel_general:
+            return
+
+        message_content = message.content.strip("<>")
+        if tiktok_match := self.tiktok_pattern.search(message_content):
+            link = tiktok_match.group(0)
+            await self.fix_tiktok(message, link) 
+        elif instagram_match := self.instagram_pattern.search(message_content):
+            link = instagram_match.group(0)
+            await self.fix_instagram(message, link)
+
+    async def fix_tiktok(self, message: discord.Message, link: str):
+        link = link.replace("www.", "")
+        link = link.replace("vm.tiktok.com/", "vm.dstn.to/")
+        link = link.replace("vt.tiktok.com/", "vm.dstn.to/")
+        link = link.replace("tiktok.com/", "vm.dstn.to/")
+
+        # get video id from link
+        await message.reply(f"I hate tiktok but here you go {link}")
+        await asyncio.sleep(0.5)
+        await message.edit(suppress=True)
+
+    async def fix_instagram(self, message: discord.Message, link: str):
+        link = link.replace("www.", "")
+        link = link.replace("instagram.com", "ddinstagram.com")
+
+        # get video id from link
+        await message.reply(f"I hate instagram but here you go {link}")
+        await asyncio.sleep(0.5)
+        await message.edit(suppress=True)
+
+
+async def setup(bot):
+    await bot.add_cog(FixSocials(bot))
