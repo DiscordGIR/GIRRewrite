f5b24ee658d912d11106455af641fd27d3c545fc
Aamir Farooq <aamir@farooq.xyz>
2023-04-04 02:37:09 +0200

    Get rid of meow cog

--- extensions.py
+++ extensions.py
@@ -18,7 +18,6 @@ initial_extensions = [
     "cogs.commands.mod.modutils",
     "cogs.monitors.misc.boosteremojis",
     "cogs.monitors.misc.fixsocials",
-    "cogs.monitors.misc.meow",
     "cogs.monitors.misc.role_assignment_buttons",
     "cogs.monitors.misc.songs",
     "cogs.monitors.mod.antiraid",
