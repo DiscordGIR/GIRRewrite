7c62ee07e7b1cd2ace39eabf8edd305453060f88
a cat <98917510+acattoXD@users.noreply.github.com>
2024-12-30 23:17:45 +0100

    changed priority + option.get("id")
    changed priority (thanks kiwi), changed it from a randomly generated uuid (yes, that worked) to whatever canister returned by using option.get("id")

--- utils/views/menus/canister.py
+++ utils/views/menus/canister.py
@@ -170,16 +170,16 @@ async def canister(ctx: GIRContext, whisper: bool, result):
 class TweakDropdown(discord.ui.Select):
     def __init__(self, author, entries, interaction, should_whisper):
         self.repo_icons = {
-            "https://repo.chariz.com": {"icon": "<:colored_sileo_tweak_icon_chariz:957470675411537980>", "priority": 1},
-            "http://apt.thebigboss.org/repofiles/cydia": {"icon": "<:colored_sileo_tweak_icon_bigboss:957470630956138496>", "priority": 2},
-            "https://repo.chimera.sh": {"icon": "<:colored_sileo_tweak_icon_chimera:957470798602440714>", "priority": 3},
-            "https://repo.dynastic.co": {"icon": "<:colored_sileo_tweak_icon_dynasti:957470833952043038>", "priority": 4},
-            "https://apt.bingner.com": {"icon": "<:colored_sileo_tweak_icon_elu:957471017578684496>", "priority": 5},
-            "https://havoc.app": {"icon": "<:colored_sileo_tweak_icon_havoc:957471075942428723>", "priority": 6},
-            "https://repo.theodyssey.dev": {"icon": "<:colored_sileo_tweak_icon_odyssey:957471133844799518>", "priority": 7},
-            "https://repo.packix.com": {"icon": "<:colored_sileo_tweak_icon_packix:957471186638479470>", "priority": 8},
-            "https://apt.procurs.us": {"icon": "<:colored_sileo_tweak_icon_procurs:957471236718460959>", "priority": 9},
-            "https://repo.twickd.com": {"icon": "<:colored_sileo_tweak_icon_twickd:957471321351135233>", "priority": 10},
+            "https://havoc.app": {"icon": "<:colored_sileo_tweak_icon_havoc:957471075942428723>", "priority": 1},
+            "https://repo.chariz.com": {"icon": "<:colored_sileo_tweak_icon_chariz:957470675411537980>", "priority": 2},
+            "http://apt.thebigboss.org/repofiles/cydia": {"icon": "<:colored_sileo_tweak_icon_bigboss:957470630956138496>", "priority": 3},
+            "https://apt.procurs.us": {"icon": "<:colored_sileo_tweak_icon_procurs:957471236718460959>", "priority": 4},
+            "https://repo.theodyssey.dev": {"icon": "<:colored_sileo_tweak_icon_odyssey:957471133844799518>", "priority": 5},
+            "https://repo.chimera.sh": {"icon": "<:colored_sileo_tweak_icon_chimera:957470798602440714>", "priority": 6},
+            "https://apt.bingner.com": {"icon": "<:colored_sileo_tweak_icon_elu:957471017578684496>", "priority": 7},
+            "https://repo.twickd.com": {"icon": "<:colored_sileo_tweak_icon_twickd:957471321351135233>", "priority": 8},
+            "https://repo.packix.com": {"icon": "<:colored_sileo_tweak_icon_packix:957471186638479470>", "priority": 9},
+            "https://repo.dynastic.co": {"icon": "<:colored_sileo_tweak_icon_dynasti:957470833952043038>", "priority": 10},
         }
         self.author = author
         self.interaction = interaction
@@ -196,7 +196,7 @@ class TweakDropdown(discord.ui.Select):
             if option.get("package") in seen_packages:
                 continue
                 
-            value = f"{index}_{option.get('uuid', str(random.randint(1000, 9999)))}"
+            value = option.get("id")
             
             repo_uri = option.get('repository', {}).get('uri')
             emoji = self.repo_icons.get(repo_uri, {}).get('icon', "<:sileo_tweak_icon:957456295898779678>")
